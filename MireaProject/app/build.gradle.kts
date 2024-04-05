dd21d0811fbac754aec4cfa29ebb4af42c805919
Frostbite <frostbca@student.21-school.ru>
2024-04-06 01:05:03 +0300

    Lesson3_Mirea_Project

--- MireaProject/app/build.gradle.kts
+++ MireaProject/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "ru.mirea.solovevave.mireaproject"
+    namespace = "ru.mirea.tmenovapa.mireaproject"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "ru.mirea.solovevave.mireaproject"
+        applicationId = "ru.mirea.tmenovapa.mireaproject"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -19,7 +19,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -38,8 +41,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.navigation:navigation-fragment:2.6.0")
-    implementation("androidx.navigation:navigation-ui:2.6.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
